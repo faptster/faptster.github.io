024cb4849516166d245245b7877990d1622c43f7
faptster <pichapongtidthian@gmail.com>
2026-01-31 21:51:06 +0700

    Update code.js

--- code.js
+++ code.js
@@ -8,7 +8,7 @@ const slides = document.querySelectorAll(".slideshow img");
 let index = 0;
 
 setInterval(() => {
-  slides[index].classList.remove("active");
-  index = (index + 1) % slides.length;
-  slides[index].classList.add("active");
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    slides[index].classList.add("active");
 }, 1500);
